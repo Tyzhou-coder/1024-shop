c01643fe72ac428784a1621de0b7caee779b1456
Tyzhou <15122936787@163.com>
2021-03-12 16:30:32 +0800

    aaaaaaaa

--- class-user-service/src/main/java/net/xdclass/UserApplication.java
+++ class-user-service/src/main/java/net/xdclass/UserApplication.java
@@ -1,5 +1,9 @@
 package net.xdclass;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * TODO
  *
@@ -7,5 +11,10 @@ package net.xdclass;
  * @version 1.0
  * @date 2021/3/12 9:53
  */
+@SpringBootApplication
+@MapperScan("net.xdclass.mapper")
 public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
 }
